f8eee7e3d7ade9a64d0885edc966474c7721fa7b
Dingo <oferdottan@gmail.com>
2020-03-01 13:40:17 +0200

    Initial upload

---
+++ Visual Studio 2019/Templates/ProjectTemplates/RimWorld_EmptyMod/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace $safeprojectname$
+{
+	public class Main
+	{
+
+	}
+}
